4f42e6d12906559fb8eebba5bc7e6d22d0d7cee3
Imacikus <techi@disroot.org>
2025-03-17 08:10:32 +0100

    Update variablen.py

--- variablen.py
+++ variablen.py
@@ -12,5 +12,14 @@ print('Differenz    :', differenz)      #Different ausgeben
 produkt = a * b                         #Produkt berechnen
 print('Produkt      :', produkt)        #Produkt ausgeben
 
-qu1 = b/a                               #Produkt berechnen
-print('Quotient     :', qu1)            #Produkt ausgeben
+qu1 = b/a                               #Quotient1 berechnen
+print('b/a          :', qu1)            #Quotient1 ausgeben
+
+qu2 = a/b                               #Quotient2 berechnen 
+print('a/b          :', qu2)            #Quotient2 ausgeben
+
+pot = b**a                              #b^a berechnen
+print('b**a         :', pot)            #b^a ausgeben
+
+p2 = b*qu2                              #Produkt 2 ist Gleitkommazahl
+print('p2           :', p2)             #Produkt 2 ausgben
