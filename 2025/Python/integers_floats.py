52599dd8fa2ebbcfadb7f908bab7f3cd5b7928e6
Imacikus <techi@disroot.org>
2025-03-25 08:51:46 +0100

    Create integers_floats.py

---
+++ 2025/Python/integers_floats.py
@@ -0,0 +1,10 @@
+#Integers und Floats
+
+import math
+
+x = int(input('Eingabe x als Ganzzahl (Integer): '))
+y = float(input('Eingabe y als Gleitkommazahl  :'))
+
+print('x = ', x)            #Resultate ausgeben
+print('y = ', y)
+print('Pi = ', math.pi)
