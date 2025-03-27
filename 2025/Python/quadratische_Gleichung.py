44b1278789082800765a9fc1e08ab2c59127fb0b
Imacikus <techi@disroot.org>
2025-03-27 08:47:53 +0100

    Update quadratische_Gleichung.py

--- 2025/Python/quadratische_Gleichung.py
+++ 2025/Python/quadratische_Gleichung.py
@@ -5,22 +5,25 @@
 import math                             # Mathe Bibliothek importieren
 
 a = float(input('a: '))                 # Variable a abfragen
-b = float(input('b: '))                 # Variable b abfragen
-c = float(input('c: '))                 # Variable c abfragen
-
-#Diskriminante berechnen
-diskriminante = b**2 - 4*a*c            
-
-if diskriminante > 0:                                           #Diskriminante größer als 0?
-    x1 = (-b + math.sqrt(diskriminante)) / (2 * a)              #ja: x1 und x2 berechnen
-    x2 = (-b - math.sqrt(diskriminante)) / (2 * a)
-    print(f"Die Lösungen sind x1 = {x1} und x2 = {x2}")         #Ergebnisse ausgeben
-
-elif diskriminante == 0:                                        #Diskriminante gleich 0?
-    x1 = -b / (2 * a)                                           #ja: x1 berechnen
-    print(f"Es gibt eine doppelte Lösung: x1 = {x1}")
-
+if a == 0:                              # Überprüfen, ob a gleich 0 ist
+    print("Fehler: 'a' darf nicht 0 sein, da sonst keine quadratische Gleichung vorliegt.")
 else:
-    print('Es gibt keine reelle Lösung')                        #nein: keine Lösung Ausgeben
+    b = float(input('b: '))             # Variable b abfragen
+    c = float(input('c: '))             # Variable c abfragen
+
+    # Diskriminante berechnen
+    diskriminante = b**2 - 4*a*c            
+
+    if diskriminante >= 0:                                           # Diskriminante größer als 0?
+        x1 = (-b + math.sqrt(diskriminante)) / (2 * a)              # ja: x1 und x2 berechnen
+        x2 = (-b - math.sqrt(diskriminante)) / (2 * a)
+        print(f"Die Lösungen sind x1 = {x1} und x2 = {x2}")         # Ergebnisse ausgeben
+
+    elif diskriminante == 0:                                        # Diskriminante gleich 0?
+        x1 = -b / (2 * a)                                           # ja: x1 berechnen
+        print(f"Es gibt eine doppelte Lösung: x1 = {x1}")
+
+    else:
+        print('Es gibt keine reelle Lösung')                        # nein: keine Lösung ausgeben
 
 
