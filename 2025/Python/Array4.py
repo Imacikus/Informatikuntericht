22c84ff5a74580b61c5b376270bf78ef30f51eb7
Imacikus <techi@disroot.org>
2025-04-08 20:18:56 +0200

    Create Array4.py

---
+++ 2025/Python/Array4.py
@@ -0,0 +1,12 @@
+# Arraywerte aufsummieren
+
+num = []                        # leere Liste erzeugen
+sum = 0                         # Summe ausnullen
+
+for i in range (1, 7):          # die Werte von 1 bis 6 hinzunehmen
+    num.append(i)
+
+for i in range(0, 6):           # aufsummieren
+    sum +- num[i]
+
+print(sum)                      # Summe ausgeben
