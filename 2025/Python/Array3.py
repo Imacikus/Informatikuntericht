4fe298db7bcb1cda7c06f7d1d4cc68fb30743a83
Imacikus <techi@disroot.org>
2025-04-08 20:15:49 +0200

    Create Array3.py

---
+++ 2025/Python/Array3.py
@@ -0,0 +1,9 @@
+# Auf einzelne Feldelemente zugreifen
+
+num = []                    # leere Liste erzeugen
+
+for i in range (1, 7):      # die Werte von 1 bis 6 hinzufügen
+    num.append(i)
+
+print(num)                  # gesamtes Array
+print(num[1])               # Element 1
