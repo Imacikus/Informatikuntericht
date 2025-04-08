805738190636986768f6e8b602eaefa80dcd3f50
Imacikus <techi@disroot.org>
2025-04-08 20:13:13 +0200

    Rename

---
+++ 2025/Python/Array2.py
@@ -0,0 +1,10 @@
+# Ein Feld automatisch mit Werten belegen
+
+num = []                        # leere Liste erzeugen
+
+print(num)                      # Kontrollausdruck
+
+for i in range (1, 7):          # die Werte von 1 bis 6 hinzufügen
+    num.append(i)
+
+print(num)                      # Kontrollausdruck
