dec07808c4d23b0330b6e3ff4d4b483afd45ce46
Imacikus <techi@disroot.org>
2025-04-08 19:47:09 +0200

    Create 17Index1.py

---
+++ 2025/Python/17Index1.py
@@ -0,0 +1,11 @@
+for index in range(5):                  # Initialisierung
+    print(index)
+
+for index in range(5):
+    print(index + 1)
+
+for index in range(1, 6):
+    print(index)
+
+for index in range(0, 11, 2):            # Schritweise 2
+    print(index)
