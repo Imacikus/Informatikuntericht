959ce3110227e8c532fb09fc48a1c1d2d5d12cc3
Imacikus <techi@disroot.org>
2025-04-28 08:30:38 +0200

    Create Array6.py

---
+++ 2025/Python/Array6.py
@@ -0,0 +1,10 @@
+# Median berechnen
+
+num = []                                    # leere Liste erzeugen
+
+for i in range(1, 8):                       # die Werte von 1 bis 7 hinzunehmen
+    num.append(i)
+
+num.sort()                                  
+
+print(num)
