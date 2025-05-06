6745dab97f64e6d8cf369b0a36f84b194cf6f360
Imacikus <techi@disroot.org>
2025-05-06 12:51:07 +0200

    update

---
+++ 2025/Python/sort-median.py
@@ -0,0 +1,16 @@
+from statistics import median  # Medianfunktion importieren
+
+# Ursprüngliche unsortierte Liste
+liste = [181, 187, 196, 196, 198, 203, 207, 211, 215]
+
+# Listen sortieren
+liste_aufsteigend = sorted(liste)           # Aufsteigend sortieren
+liste_absteigend = sorted(liste, reverse=True)  # Absteigend sortieren
+
+# Median berechnen
+median_wert = median(liste)
+
+# Ergebnisse ausgeben
+print("Aufsteigend sortiert:", liste_aufsteigend)
+print("Absteigend sortiert:", liste_absteigend)
+print("Median:", median_wert)
---
+++ 2025/Python/sort.py
@@ -0,0 +1,8 @@
+liste1 = [100, 600, 400, 8000, 50]          #unsortierte Liste
+liste2 = [100, 600, 400, 8000, 50]
+
+liste1.sort()                               #Liste aufsteigend
+liste2.sort(reverse=True)                   #Liste absteigend  
+
+print("Aufsteigend:", liste1)
+print("Absteigen:", liste2)
