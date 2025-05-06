c8301afb0c07b5648fc131754f6bdc1210163fa6
Imacikus <techi@disroot.org>
2025-05-06 07:56:48 +0200

    Create mean.py

---
+++ 2025/Python/mean.py
@@ -0,0 +1,8 @@
+from statistics import mean                         # Mittelwert funktion imporieren
+
+# E folgt das zu mittelnde Feld:
+
+zahlen = [19, 22, 34, 26, 32, 30, 24, 24]
+
+am = mean(zahlen)                                   # Arithmetisches Mittel berechnen
+print("Arithmetisches Mittel =", am)
