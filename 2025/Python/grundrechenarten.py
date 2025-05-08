e3ee4c6d64a17ced8a87345750962d42d77bf38e
Imacikus <techi@disroot.org>
2025-05-08 08:08:37 +0200

    Update grundrechenarten.py

--- 2025/Python/grundrechenarten.py
+++ 2025/Python/grundrechenarten.py
@@ -11,12 +11,27 @@
 def addi(a, b):
     return a + b
 
+def diferenz(a, b):
+    return a - b
+
+def produkt(a, b):
+    return a * b
+
+def devi(a, b):
+    return a / b
+
 # Werte in der Konsole abfragen
-a = float(input("Summand 1: "))
-b = float(input("Summand 2: "))
+a = float(input("Zahl 1: "))
+b = float(input("Zahl 2: "))
 
 # Funktionsaufruf im Hauptprogramm
 Summe = addi(a, b)
+Differenz = diferenz(a, b)
+Produkt = produkt(a, b)
+Dividenz = devi(a, b)
 
 # Resultat printen
-print("Ergebnis Addition =", Summe)
+print("Summe =", Summe)
+print("Differenz =", Differenz)
+print("Produkt =", Produkt)
+print("Dividenz =", Dividenz)
