0dbe58ac38466fef41094cdeba7090417e766c1b
Imacikus <techi@disroot.org>
2025-06-19 08:18:54 +0200

    Update camebridge1.py

--- 2025/Python/camebridge1.py
+++ 2025/Python/camebridge1.py
@@ -1,8 +1,8 @@
-import math
+import math                                                 # Mathe Paket importieren
 
-result1 = math.pow(5, 1/3)
-zwischenwert = math.pow(40, 1/3) + math.pow(135, 1/3)
-result2 = math.pow(zwischenwert, 1/4)
+result1 = math.pow(5, 1/3)                                  # Ergebnis 1 berechnen
+zwischenwert = math.pow(40, 1/3) + math.pow(135, 1/3)       # zwischenergebnis berechnen
+result2 = math.pow(zwischenwert, 1/4)                       # Die Vierte Wurzel vom Zwischenergebnis berechnen
 
-print("3te Wurzel von 5 ", result1)
+print("3te Wurzel von 5 ", result1)                         # Printen
 print("Original Aufgabe ", result2)
