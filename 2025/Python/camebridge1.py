5d7b7f5ad7bc5ed3e15892a330446773327a5de1
Imacikus <techi@disroot.org>
2025-06-19 08:12:27 +0200

    Create camebridge1.py

---
+++ 2025/Python/camebridge1.py
@@ -0,0 +1,8 @@
+import math
+
+result1 = math.pow(5, 1/3)
+zwischenwert = math.pow(40, 1/3) + math.pow(135, 1/3)
+result2 = math.pow(zwischenwert, 1/4)
+
+print("3te Wurzel von 5 ", result1)
+print("Original Aufgabe ", result2)
