4cc78955c6dc7218f1270ad4873388a27eb7f98b
Imacikus <techi@disroot.org>
2025-11-06 10:32:59 +0100

    Update schueler_class.py

--- Klasse 10/Python/schueler_class.py
+++ Klasse 10/Python/schueler_class.py
@@ -1,26 +1,36 @@
-#schueler_class.py
+# schueler_class.py
 
-class schueler:
-    #Klasse für einen Schüler mit Name, Alter, Größe, Hobby
-
-    def __init__(self, name, alter, groesse, hobby,nivau):
+class Schueler:
+    # Klasse für einen Schüler mit Name, Alter, Größe, Hobby und Niveau
+    def __init__(self, name, alter, groesse, klasse, hobby):
         self.name = name
         self.alter = alter
         self.groesse = groesse
+        self.klasse = klasse
         self.hobby = hobby
-        self.nivau = nivau
 
     def vorstellen(self):
-        print(f"Hallo ich bin {self.name}.")
+        print(f"Hallo, ich bin {self.name}.")
         print(f"Ich bin {self.alter} Jahre alt.")
-        print(f"Ich bin {self.groesse}m Groß.")
+        print(f"Ich bin {self.groesse}m groß.")
+        print(f"Ich bin in der Klasse {self.klasse}.")
         print(f"Mein Hobby ist {self.hobby}.")
-        print(f"Mein Nivau ist {self.nivau}")
+        print()
 
-# Instanzen einstellen
+    def geburtstag(self):
+        self.alter += 1
+        print(f"Alles Gute zum Geburtstag, {self.name}! 🎉")
+        print(f"Du bist jetzt {self.alter} Jahre alt.\n")
 
-schueler1 = schueler("Max Mustermann", 16, 1.80, "10c", "Programmieren")
-schueler2 = schueler("Max Musterfrau", 15, 1.71, "11c", "Tanzen")
 
+# Instanzen erstellen
+schueler1 = Schueler("Max Mustermann", 16, 1.80, "10c", "Programmieren")
+schueler2 = Schueler("Max Musterfrau", 15, 1.71, "11c", "Tanzen")
+
+# Methoden aufrufen
 schueler1.vorstellen()
 schueler2.vorstellen()
+
+# Geburtstag
+schueler1.geburtstag()
+schueler2.geburtstag()
